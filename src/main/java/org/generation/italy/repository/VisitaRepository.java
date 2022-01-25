2739e88e3452156a79b3fd81a533f3ebe6ad71f6
saviodambrosio <saviodambrosio@gmail.com>
2022-01-25 16:47:16 +0100

    repo

--- src/main/java/org/generation/italy/repository/VisitaRepository.java
+++ src/main/java/org/generation/italy/repository/VisitaRepository.java
@@ -1,9 +1,10 @@
 package org.generation.italy.repository;
 
+import org.generation.italy.model.Visita;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VisitaRepository extends JpaRepository<VisitaRepository, Integer> {
+public interface VisitaRepository extends JpaRepository<Visita, Integer> {
 
 }
