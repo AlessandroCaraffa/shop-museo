54a587d1db221d3a334c3df6e5df3c8b65562c76
ABozzoli <alessandrobozzoli92@gmail.com>
2022-01-25 16:32:48 +0100

    Agg. relazioni foto-prodotto-percorso

--- src/main/java/org/generation/italy/model/Prodotto.java
+++ src/main/java/org/generation/italy/model/Prodotto.java
@@ -7,7 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
@@ -26,42 +26,33 @@ public class Prodotto {
 	@NotNull
 	private BigDecimal prezzoVendita;
 	
-	@OneToMany(mappedBy="prodotto")
+	@ManyToMany
 	private List<Foto> foto;
 	
 	// getters/setters
 	public Integer getId() {
 		return id;
 	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 	public String getDescrizione() {
 		return descrizione;
 	}
-
 	public void setDescrizione(String descrizione) {
 		this.descrizione = descrizione;
 	}
-
 	public BigDecimal getPrezzoVendita() {
 		return prezzoVendita;
 	}
-
 	public void setPrezzoVendita(BigDecimal prezzoVendita) {
 		this.prezzoVendita = prezzoVendita;
 	}
-	
-
 	
 }
