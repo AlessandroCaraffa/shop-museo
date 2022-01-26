e8ba392265c4610291a13c0630f5f9677347b943
Brun096 <Brunoh96@outlook.it>
2022-01-26 11:53:35 +0100

    modifica FotoService

--- src/main/java/org/generation/italy/service/FotoService.java
+++ src/main/java/org/generation/italy/service/FotoService.java
@@ -1,8 +1,10 @@
 package org.generation.italy.service;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.generation.italy.model.Foto;
+import org.generation.italy.model.FotoForm;
 import org.generation.italy.repository.FotoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,8 +15,25 @@ public class FotoService {
 	@Autowired
 	private FotoRepository repository;
 	
+	
+	public Foto create(FotoForm newFoto) throws IOException{
+		Foto photo = new Foto();
+		photo.setTitolo(newFoto.getTitolo());
+		if(newFoto.getContenuto() != null) {
+			byte[] contentSerialized = newFoto.getContenuto().getBytes();
+			photo.setContenuto(contentSerialized);
+		}
+		
+		return repository.save(photo);
+	};
+	
 	public List<Foto> findAll(){
 		return repository.findAll();
+	}
+	
+
+	public Foto getById(Integer id) {
+		return repository.getById(id);
 	}
 
 }
