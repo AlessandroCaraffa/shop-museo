ba904e964d6cb780822c1bb58f5c88a48c6a8dc0
AlessandroCaraffa <alessandro.caraffa93@gmail.com>
2022-01-27 11:29:29 +0100

    inizio edit vendite

--- src/main/java/org/generation/italy/controller/VenditaController.java
+++ src/main/java/org/generation/italy/controller/VenditaController.java
@@ -1,23 +1,53 @@
 package org.generation.italy.controller;
 
+import javax.validation.Valid;
+
+import org.generation.italy.model.Guida;
+import org.generation.italy.model.Vendita;
+import org.generation.italy.service.ProdottoService;
 import org.generation.italy.service.VenditaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/9")
+@RequestMapping("/vendite")
 public class VenditaController {
 	
 	@Autowired
 	private VenditaService service;
+	
+	@Autowired ProdottoService prodottoService;
 
 	@GetMapping
 	public String list(Model model) {
 		model.addAttribute("list", service.findAll());
 		return "";
+	}
+	
+	@GetMapping("/create")
+	public String create(Model model) {
+		model.addAttribute("edit", false);
+		model.addAttribute("prodotti", prodottoService.findAllSortedByNome());
+		model.addAttribute("guida", new Vendita());
+		return "/vendite/edit";
+	}
+	
+	@PostMapping("/create")
+	public String doCreate(@Valid @ModelAttribute("vendita") Vendita formGuida, BindingResult bindingResult, Model model) {
+		if(bindingResult.hasErrors()) {
+			model.addAttribute("edit", false);
+
+			return "/guide/edit";
+
+		}
+		service.save(formGuida);
+		return "redirect:/guide";
 	}
 	
 }
--- src/main/java/org/generation/italy/service/VenditaService.java
+++ src/main/java/org/generation/italy/service/VenditaService.java
@@ -2,6 +2,7 @@ package org.generation.italy.service;
 
 import java.util.List;
 
+
 import org.generation.italy.model.Vendita;
 import org.generation.italy.repository.VenditaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,9 @@ public class VenditaService {
 	
 	public List<Vendita> findAll(){
 		return repository.findAll();
+	}
+	public Vendita save(Vendita vendita) {
+		return repository.save(vendita);
 	}
 
 }
