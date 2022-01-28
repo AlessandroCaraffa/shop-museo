f1e143fe75e5cf2517617e7e2620c8b7c233957e
GianlucaPensa <janjyboy2000@gmail.com>
2022-01-28 12:13:33 +0100

    acquisto base

--- src/main/java/org/generation/italy/service/AcquistoProdottoService.java
+++ src/main/java/org/generation/italy/service/AcquistoProdottoService.java
@@ -16,5 +16,21 @@ public class AcquistoProdottoService {
 	public List<AcquistoProdotto> findAll(){
 		return repository.findAll();
 	}
+	
+	public AcquistoProdotto save(AcquistoProdotto aProdotto) {
+		return repository.save(aProdotto);
+	}
+	
+	public void deleteById(Integer id) {
+		repository.deleteById(id);
+	}
+	
+	public AcquistoProdotto getById(Integer id) {
+		return repository.getById(id);
+	}
+	
+	public AcquistoProdotto update(AcquistoProdotto aProdotto) {
+		return repository.save(aProdotto);
+	}
 
 }
