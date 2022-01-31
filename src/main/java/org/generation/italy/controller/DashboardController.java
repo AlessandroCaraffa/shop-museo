88d39ce4842ec861d54ab0f33d7b813752e81c78
saviodambrosio <saviodambrosio@gmail.com>
2022-01-31 16:55:54 +0100

    dash

---
+++ src/main/java/org/generation/italy/controller/DashboardController.java
@@ -0,0 +1,18 @@
+package org.generation.italy.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/dashboard")
+public class DashboardController {
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/org/generation/italy/model/Dashboard.java
@@ -0,0 +1,19 @@
+package org.generation.italy.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Dashboard {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	
+	
+	
+	
+	
+}
