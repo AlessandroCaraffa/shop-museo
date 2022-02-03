d0ccddca776c07ca9fa1a5bd9da47adee7cfc64d
GianlucaPensa <janjyboy2000@gmail.com>
2022-02-03 10:15:35 +0100

    acquistoProdotto delete2

--- src/main/java/org/generation/italy/controller/AcquistoProdottoController.java
+++ src/main/java/org/generation/italy/controller/AcquistoProdottoController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,6 +31,13 @@ public class AcquistoProdottoController {
 		service.save(aProdotto, id);
 		String url = "redirect:/acquisti/" + id.toString();
 		return url;
+	}
+	
+	@GetMapping("acquisti/delete/{id}/{acquistoProdottoId}")
+	public String doDelete(Model model, @PathVariable("id") Integer id,@PathVariable("acquistoProdottoId") Integer acquistoProdottoId) {
+		service.deleteById(acquistoProdottoId);
+		return "redirect:/acquisti/{id}";
+
 	}
 	
 }
