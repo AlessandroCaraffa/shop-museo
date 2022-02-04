b50a5a45c69ddbb07cdf91e0a6c06ef2a26283c2
Brun096 <Brunoh96@outlook.it>
2022-02-04 09:00:16 +0100

    hoem

--- src/main/java/org/generation/italy/controller/HomeController.java
+++ src/main/java/org/generation/italy/controller/HomeController.java
@@ -1,5 +1,6 @@
 package org.generation.italy.controller;
 
+import org.generation.italy.service.GuidaService;
 import org.generation.italy.service.PercorsoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,10 +14,13 @@ public class HomeController {
 
 	@Autowired
 	private PercorsoService service;
+	@Autowired
+	private GuidaService guidaService;
 	
 	@GetMapping
 	public String list(Model model) {
 		model.addAttribute("percorsi", service.findAll());
+		model.addAttribute("guide", guidaService.findAllSortByCognome());
 		return"/home";
 	}
 }
